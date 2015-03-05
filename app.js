e965b35416072d4980b4eb8c946e400e3b7b9834
unloop <pastor.konstantin@gmail.com>
2015-03-05 20:12:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+(function () {
+  'use strict';
+  var app = express();
 
+  app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
 
-var server = app.listen(3000, function () {
+  var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-
-});
+    console.log('Example app listening at http://%s:%s', host, port);
+  });
+})();
